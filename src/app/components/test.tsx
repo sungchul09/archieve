417767e3242e76a1b8e13f182d754f02e50470b8
sungchul09 <sungchul09@naver.com>
2023-08-10 04:09:29 +0900

    feat: package install

---
+++ src/app/components/test.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function TestComponent() {
+  return (
+    <div>
+      <div>
+        <p>asdf</p>
+      </div>
+    </div>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,13 +10,13 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
     </html>
   )
 }
