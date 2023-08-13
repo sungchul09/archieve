e7b46e6be04e3eb1516cfa0c2528f09b0fc0fe89
sungchul09 <sungchul09@naver.com>
2023-08-13 19:29:25 +0900

    feat: tailwind px 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,9 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import type { Config } from 'tailwindcss'
 
+const px0_10 = { ...Array.from(Array(11)).map((_, i) => `${i}px`) }
+const px0_100 = { ...Array.from(Array(101)).map((_, i) => `${i}px`) }
+const px0_200 = { ...Array.from(Array(201)).map((_, i) => `${i}px`) }
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -8,10 +12,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      borderWidth: px0_10 as any,
+      fontSize: px0_100 as any,
+      lineHeight: px0_100 as any,
+      minWidth: px0_200 as any,
+      minHeight: px0_200 as any,
+      spacing: px0_200 as any,
+      borderRadius: px0_200 as any,
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
